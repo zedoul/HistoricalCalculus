510ca592906c64434e0ec512ebb57a1e3570fa4d
Kim Seonghyun <shyeon.kim@scipi.net>
2014-09-28 15:44:50 +0200

    Fixed docs (2)
    Signed-off-by: Kim Seonghyun <shyeon.kim@scipi.net>

--- test.py
+++ test.py
@@ -7,15 +7,12 @@ def UK_measure(amount, fromyear, toyear):
     d2 = histcalc.UK_EarningIndex_measure(amount, fromyear, toyear)
     d3 = histcalc.UK_GDPperCapita_measure(amount, fromyear, toyear)
 
-#    print "The price of " + str(amount) + "£ in " + str(fromyear)
-#    print ""
-#    print "In " + str(toyear) + " :"
     print "The price of " + str(amount) + "£ in " + str(fromyear)
     print "=============================="
     def price(x): return '%1.2f'%x
-    print "real price based on RPI is " + str(price(d1)) + "£, 한화 " +str(price(1696.96*d1)) +"₩"
-    print "labour price based on wage is " + str(price(d2)) + "£, 한화 " +str(price(1696.96*d2)) +"₩"
-    print "income price based on GDP is " + str(price(d3)) + "£, 한화 " +str(price(1696.96*d3)) +"₩"
+    print "- Real price based on RPI is " + str(price(d1)) + "£, 한화 " +str(price(1696.96*d1)) +"₩"
+    print "- Labour price based on wage is " + str(price(d2)) + "£, 한화 " +str(price(1696.96*d2)) +"₩"
+    print "- Income price based on GDP is " + str(price(d3)) + "£, 한화 " +str(price(1696.96*d3)) +"₩"
 
 def JP_measure(amount, fromyear, toyear):
     d1 = histcalc.JP_CPI_measure(amount, fromyear, toyear)
@@ -23,7 +20,7 @@ def JP_measure(amount, fromyear, toyear):
     print "The price of " + str(amount) + "¥ in " + str(fromyear)
     print "=============================="
     def price(x): return '%1.2f'%x
-    print "real price based on CPI is " + str(price(d1)) + "¥, 한화 "+str(price(9.56*d1))+"₩"
+    print "- Real price based on CPI is " + str(price(d1)) + "¥, 한화 "+str(price(9.56*d1))+"₩"
 
 for i in range(1830, 2001):
     UK_measure(1, i, 2013)
