5e4446a75d907f8d5e5d158f55329128860c8aae
Kim Seonghyun <shyeon.kim@scipi.net>
2014-09-28 15:39:05 +0200

    Fixed docs
    Signed-off-by: Kim Seonghyun <shyeon.kim@scipi.net>

--- test.py
+++ test.py
@@ -7,9 +7,11 @@ def UK_measure(amount, fromyear, toyear):
     d2 = histcalc.UK_EarningIndex_measure(amount, fromyear, toyear)
     d3 = histcalc.UK_GDPperCapita_measure(amount, fromyear, toyear)
 
+#    print "The price of " + str(amount) + "£ in " + str(fromyear)
+#    print ""
+#    print "In " + str(toyear) + " :"
     print "The price of " + str(amount) + "£ in " + str(fromyear)
-    print ""
-    print "In " + str(toyear) + " :"
+    print "=============================="
     def price(x): return '%1.2f'%x
     print "real price based on RPI is " + str(price(d1)) + "£, 한화 " +str(price(1696.96*d1)) +"₩"
     print "labour price based on wage is " + str(price(d2)) + "£, 한화 " +str(price(1696.96*d2)) +"₩"
@@ -19,15 +21,12 @@ def JP_measure(amount, fromyear, toyear):
     d1 = histcalc.JP_CPI_measure(amount, fromyear, toyear)
 
     print "The price of " + str(amount) + "¥ in " + str(fromyear)
-    print ""
-    print "In " + str(toyear) + " :"
+    print "=============================="
     def price(x): return '%1.2f'%x
     print "real price based on CPI is " + str(price(d1)) + "¥, 한화 "+str(price(9.56*d1))+"₩"
 
 for i in range(1830, 2001):
-    print "-------------------------------"
     UK_measure(1, i, 2013)
 for i in range(1879, 2001):
-    print "-------------------------------"
     JP_measure(1, i, 2011)
 
