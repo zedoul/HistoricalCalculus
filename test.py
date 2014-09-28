98fff82e265c7b3e8c83419c13eff41ade6cd375
Kim Seonghyun <shyeon.kim@scipi.net>
2014-09-28 15:46:44 +0200

    Fixed docs (3)
    Signed-off-by: Kim Seonghyun <shyeon.kim@scipi.net>

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ def UK_measure(amount, fromyear, toyear):
     print "- Real price based on RPI is " + str(price(d1)) + "£, 한화 " +str(price(1696.96*d1)) +"₩"
     print "- Labour price based on wage is " + str(price(d2)) + "£, 한화 " +str(price(1696.96*d2)) +"₩"
     print "- Income price based on GDP is " + str(price(d3)) + "£, 한화 " +str(price(1696.96*d3)) +"₩"
+    print ""
 
 def JP_measure(amount, fromyear, toyear):
     d1 = histcalc.JP_CPI_measure(amount, fromyear, toyear)
@@ -21,6 +22,7 @@ def JP_measure(amount, fromyear, toyear):
     print "=============================="
     def price(x): return '%1.2f'%x
     print "- Real price based on CPI is " + str(price(d1)) + "¥, 한화 "+str(price(9.56*d1))+"₩"
+    print ""
 
 for i in range(1830, 2001):
     UK_measure(1, i, 2013)
